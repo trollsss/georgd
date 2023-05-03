5ffb6643c45d975a9cde3a7cd037a916119d792f
SemenovG <semenovg@iMac-Administrator.local>
2023-05-03 12:37:51 +0900

    first commit

--- app/src/main/java/com/example/kurs/MainActivity.java
+++ app/src/main/java/com/example/kurs/MainActivity.java
@@ -3,13 +3,28 @@ package com.example.kurs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+
+import android.os.Handler;
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.analiz);
+
+
     }
-    class S
+
 }
